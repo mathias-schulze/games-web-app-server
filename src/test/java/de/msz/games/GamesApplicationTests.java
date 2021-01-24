27cd86f456657d5491df02a001b03e7436eec947
Mathias Schulze <mathias-schulze@web.de>
2021-01-24 16:48:42 +0100

    Spring Boot Initializr

---
+++ src/test/java/de/msz/games/GamesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.msz.games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
