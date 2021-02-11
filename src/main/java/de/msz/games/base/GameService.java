5138f1697177c6d5bd5e0835b05838b910bb16c5
Mathias Schulze <mathias-schulze@web.de>
2021-02-11 23:49:15 +0100

    list of active games

--- src/main/java/de/msz/games/base/GameService.java
+++ src/main/java/de/msz/games/base/GameService.java
@@ -1,6 +1,7 @@
 package de.msz.games.base;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,11 +14,14 @@ import org.springframework.stereotype.Component;
 
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.QuerySnapshot;
 
 import de.msz.games.base.Counter.CounterName;
 import de.msz.games.base.firebase.FirebaseService;
 import de.msz.games.base.firebase.FirebaseService.FirestoreCollectionName;
+import lombok.Data;
 
 @Component
 public class GameService {
@@ -41,8 +45,8 @@ public class GameService {
 	public String createNewGame(Game game) throws InterruptedException, ExecutionException {
 		
 		Map<String, Object> newGameValues = new HashMap<>();
-		newGameValues.put("no", "" + counter.getNextValue(CounterName.GAME));
-		newGameValues.put("created", "" + System.currentTimeMillis());
+		newGameValues.put("no", counter.getNextValue(CounterName.GAME));
+		newGameValues.put("created", System.currentTimeMillis());
 		newGameValues.put("game", "" + game.getParameter().getId());
 		newGameValues.put("stage", "" + Stage.NEW.name());
 		
@@ -54,5 +58,36 @@ public class GameService {
 				firestore.collection(FirestoreCollectionName.GAMES.getName()).add(newGameValues);
 		
 		return newGameFuture.get().getId();
+	}
+	
+	public List<ActiveGame> getActiveGames() throws InterruptedException, ExecutionException {
+		
+		ApiFuture<QuerySnapshot> activeGamesFuture = 
+				firestore.collection(FirestoreCollectionName.GAMES.getName())
+					.whereIn("stage", Arrays.asList(Stage.NEW.name(), Stage.RUNNING.name()))
+					.orderBy("created")
+					.get();
+		
+		List<ActiveGame> activeGames = new ArrayList<>();
+		
+		for (DocumentSnapshot gameDocument : activeGamesFuture.get().getDocuments()) {
+			String id = gameDocument.getId();
+			Long no = gameDocument.getLong("no");
+			Long created = gameDocument.getLong("created");
+			Game game = Game.valueOf(gameDocument.getString("game"));
+			
+			activeGames.add(new ActiveGame(id, no, created, game.getParameter().getName()));
+		}
+		
+		return activeGames;
+	}
+	
+	@Data
+	public static class ActiveGame {
+		
+		private final String id;
+		private final Long no;
+		private final Long created;
+		private final String game;
 	}
 }
--- src/main/java/de/msz/games/base/GamesController.java
+++ src/main/java/de/msz/games/base/GamesController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.msz.games.base.Game.GameParameter;
+import de.msz.games.base.GameService.ActiveGame;
 import lombok.Data;
 
 @RestController
@@ -52,5 +53,10 @@ public class GamesController {
 	public static class CreateNewGameResponse {
 		
 		private String gameId;
+	}
+	
+	@GetMapping("/active")
+	public List<ActiveGame> getActiveGames() throws InterruptedException, ExecutionException {
+		return gameService.getActiveGames();
 	}
 }
