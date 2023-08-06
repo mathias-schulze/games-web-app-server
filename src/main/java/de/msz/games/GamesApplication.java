76f20bde0995a43780a537f4db6066e883c71cbb
Mathias Schulze <mathias-schulze@web.de>
2023-08-06 18:41:52 +0200

    Backup

--- src/main/java/de/msz/games/GamesApplication.java
+++ src/main/java/de/msz/games/GamesApplication.java
@@ -2,8 +2,10 @@ package de.msz.games;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class GamesApplication {
 
 	public static void main(String[] args) {
